f62ac3e415d615108245b2a608254134580a7ed3
Brett Wagner <wagnerb@udel.edu>
2020-05-26 11:23:44 -0400

    Initial Commit -Added MVC.

---
+++ src/main/Controller.java
@@ -0,0 +1,14 @@
+package main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Controller extends Application {
+	private Model model;
+	private View view;
+	@Override
+	public void start(Stage theStage) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+}
---
+++ src/main/View.java
@@ -0,0 +1,31 @@
+package main;
+
+public class View {
+	
+	/**
+	 * Height of Keyboard
+	 */
+	private double height = 545;
+	
+	/**
+	 * Width of Keyboard
+	 */
+	private double width = 1024;
+
+	/**
+	 * Getter
+	 * @return Height of Keyboard
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * Getter
+	 * @return Width of Keyboard
+	 */
+	public double getWidth() {
+		return width;
+	}
+
+}
